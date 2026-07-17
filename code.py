df006f43de7a289bfccd78d83333fec7514d8be5
Prashik Khoteja <prashikkhoteja1@gmail.com>
2026-07-17 16:35:50 +0545

    Code Added

--- code.py
+++ code.py
@@ -1,4 +1,4 @@
-# Program to find if the given number is odd or even.
+# Program to find whether the given number is odd or even.
 
 number = int(input("Enter a positive integer: "))
 if number % 2 == 0:
