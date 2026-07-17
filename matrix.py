7e26fc79025016d2a0c513748dc0815011e61ea7
Prashik Khoteja <prashikkhoteja1@gmail.com>
2026-07-17 20:03:19 +0545

    2*2 Matrix

---
+++ matrix.py
@@ -0,0 +1,7 @@
+# To print a 2*2 matrix
+a11 = int(input("Enter the elements for a11: "))
+a12 = int(input("Enter the elements for a12: "))
+a21 = int(input("Enter the elements for a21: "))
+a22 = int(input("Enter the elements for a22: "))
+print(f"{a11} {a12}")
+print(f"{a21} {a22}")
