daeb3bf1c0eeea0cdeae71c132d3d371e03c709d
Prashik Khoteja <prashikkhoteja1@gmail.com>
2026-07-17 15:58:40 +0545

    Added Code

--- code.py
+++ code.py
@@ -1,1 +1,7 @@
-print("Hello")
+# Program to find if the given number is odd or even.
+
+number = int(input("Enter a positive integer: "))
+if number % 2 == 0:
+    print("The number is even. ")
+else:
+    print("The number is odd. ")
