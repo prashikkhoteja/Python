95302ee6e207ba2acb19b17e995759fa15f0f021
Prashik Khoteja <prashikkhoteja1@gmail.com>
2026-07-22 21:51:55 +0545

    Added code

---
+++ savetofile.py
@@ -0,0 +1,11 @@
+students = {}
+
+for a in range(2):
+    name = input("Name: ")
+    marks = int(input("Marks: "))
+    students[name] = marks
+
+with open("Students.txt", "w") as file:
+
+    for name, marks in students.items():
+        file.write(f"{name},{marks} \n")
