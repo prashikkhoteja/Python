eeaf5614cb819dffccf7f0b04994f063e1338182
Prashik Khoteja <prashikkhoteja1@gmail.com>
2026-07-24 09:41:03 +0545

    Added Codes

--- word.py
+++
@@ -1,5 +0,0 @@
-# To print the  desired word for desired number of times.
-word = input("Enter a word: ")
-i = int(input(f"Enter the number of times you want to print {word} : "))
-for word in range(i):
-    print("word")
